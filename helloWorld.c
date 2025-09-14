9b933427b7639fdd038752771bbdf875fee0ca3e
Aldro2003 <juanalejandrovargasc2a@gmail.com>
2025-09-14 11:01:13 +0200

    Sesion1y2

---
+++ helloWorld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hi there!");
+  return 0;
+}
+
+// COMPILAR
+// gcc helloWorld.c -o helloWorld.exe
+
+// COMPILAR TOCHO
+//  gcc helloWorld.c -o helloWorld.exe -std=c17 -Wall -Wextra -Wpedantic
+
+// EJECUTAR
+//  .\helloWorld
