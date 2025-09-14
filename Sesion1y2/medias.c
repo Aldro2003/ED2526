b2d7b849bdd5e13c595b6a48e3dae133bcec0ea8
Aldro2003 <juanalejandrovargasc2a@gmail.com>
2025-09-14 13:05:43 +0200

    act

--- Sesion1y2/medias.c
+++ Sesion1y2/medias.c
@@ -6,19 +6,17 @@ int main()
   double numeros[100]; // Array para almacenar los números
   double suma = 0.0;
 
-  printf("¿Cuantos numeros desea introducir? ");
-  scanf("%d", &n);
-
-  if (n > 100 || n <= 0)
+  while (n > 100 || n <= 0)
   {
-    printf("Numero de elementos no valido. Debe ser entre 1 y 100.\n");
-    return 1;
+    printf("¿Cuantos numeros desea introducir? (Debe ser entre 1 y 100) ");
+    scanf("%d", &n);
   }
+
+  printf("Introduce los numeros: ");
 
   // Leer los números y almacenarlos en el array
   for (int i = 0; i < n; i++)
   {
-    printf("Numero %d: ", i + 1);
     scanf("%lf", &numeros[i]);
     suma += numeros[i]; // Sumar los números mientras se leen
   }
